2e5c14939f59db0f4ec1c8cd7be5905f9efadfc3
QuickSquirrel <101573169+QuickSquirrel@users.noreply.github.com>
2022-04-11 22:56:08 +0300

    Update init.pp

--- modules/master_init/manifests/init.pp
+++ modules/master_init/manifests/init.pp
@@ -11,11 +11,11 @@
   
  nginx::resource::server { 'static':
    listen_port => 8080,
-   proxy       => 'http://192.168.50.10:80',
+   proxy       => 'http://192.168.50.10',
  }
 
  nginx::resource::server { 'dynamic':
    listen_port => 8081,
-   proxy       => 'http://192.168.50.20:80',
+   proxy       => 'http://192.168.50.20',
  }
 }
