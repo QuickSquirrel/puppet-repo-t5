49c569182d693e7916c765b25d73cf4156b17ba5
QuickSquirrel <101573169+QuickSquirrel@users.noreply.github.com>
2022-04-11 21:48:29 +0300

    Create init.php

---
+++ modules/slave1_init/manifests/init.php
@@ -0,0 +1,25 @@
+class slave1_init {
+
+ package { 'httpd':
+  ensure => latest,
+ }
+
+ file {'/var/www/html/index.html':
+  ensure => file,
+  source => 'puppet:///modules/static/index.html'
+ }
+
+ service { 'httpd':
+  ensure => running,
+  enable => true,
+ }
+
+ service { 'firewalld':
+  ensure => stopped,
+  enable => false,
+ }
+
+ file {'/root/README':
+  ensure => absent,
+ }
+]
