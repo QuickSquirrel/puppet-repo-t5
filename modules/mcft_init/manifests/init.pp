71f4f3d602702bb5b3eeee944e575fea33431c20
QuickSquirrel <101573169+QuickSquirrel@users.noreply.github.com>
2022-04-13 12:20:57 +0300

    Update init.pp

--- modules/mcft_init/manifests/init.pp
+++ modules/mcft_init/manifests/init.pp
@@ -1,1 +1,14 @@
-q
+class mcft_init {
+
+ package {'java-17-openjdk':
+  ensure => installed
+ }
+ 
+ file {'/opt/minecraft':
+  ensure => directory
+ }
+ 
+ file {'/opt/minecraft/eula.txt':
+  content => 'eula=true',
+ }
+}
